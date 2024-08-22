0d339c973c75974d6402d052bcb6fd6c022fa1ed
GingerbreadFred <richiwatson@gmail.com>
2024-08-22 22:59:57 +0100

    Fix cpp tab issues and change size of the dispatch window

--- init.lua
+++ init.lua
@@ -883,7 +883,7 @@ require('lazy').setup({
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
         additional_vim_regex_highlighting = { 'ruby' },
       },
-      indent = { enable = true, disable = { 'ruby' } },
+      indent = { enable = true, disable = { 'ruby', 'c', 'cpp' } },
     },
     config = function(_, opts)
       -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
--- lua/custom/plugins/dispatch.lua
+++ lua/custom/plugins/dispatch.lua
@@ -1,5 +1,8 @@
 return {
   {
     'tpope/vim-dispatch',
+    config = function()
+      vim.g.dispatch_quickfix_height = 20
+    end,
   },
 }
